24102843bb875ab1f037bf75812c3a3c7b78b0b5
yinz <uinz.cn@gmail.com>
2019-02-26 12:57:12 +0800

    Update dynamic.d.ts

--- dynamic.d.ts
+++ dynamic.d.ts
@@ -1,66 +1,66 @@
 namespace dynamics {
-    type Type =
-      | typeof spring
-      | typeof linear
-      | typeof gravity
-      | typeof easeIn
-      | typeof easeInOut
-      | typeof easeOut
-      | typeof bounce
-      | typeof bezier
-      | typeof forceWithGravity;
+type Type =
+  | typeof spring
+  | typeof linear
+  | typeof gravity
+  | typeof easeIn
+  | typeof easeInOut
+  | typeof easeOut
+  | typeof bounce
+  | typeof bezier
+  | typeof forceWithGravity;
 
-    interface Options<P, T> {
-      duration?: number;
-      type?: T;
-      complete?(): void;
-      change?(target: P, progress: number): void;
-    }
+interface Options<P, T> {
+  duration?: number;
+  type?: T;
+  complete?(): void;
+  change?(target: P, progress: number): void;
+}
 
-    function animate<P, T extends Type = typeof easeInOut>(
-      target: P,
-      properties: P,
-      options?: Options<P, T> & Parameters<T>[0]
-    ): number;
+function animate<P, T extends Type = typeof easeInOut>(
+  target: P,
+  properties: P,
+  options?: Options<P, T> & Parameters<T>[0]
+): number;
 
-    function spring(config?: {
-      frequency?: number;
-      friction?: number;
-      anticipationSize?: number;
-      anticipationStrength?: number;
-    }): void;
+function spring(config?: {
+  frequency?: number;
+  friction?: number;
+  anticipationSize?: number;
+  anticipationStrength?: number;
+}): void;
 
-    function bounce(config?: { frequency?: number; friction?: number }): void;
+function bounce(config?: { frequency?: number; friction?: number }): void;
 
-    function forceWithGravity(config?: {
-      bounciness?: number;
-      elasticity?: number;
-    }): void;
+function forceWithGravity(config?: {
+  bounciness?: number;
+  elasticity?: number;
+}): void;
 
-    function gravity(config?: {
-      bounciness?: number;
-      elasticity?: number;
-    }): void;
+function gravity(config?: {
+  bounciness?: number;
+  elasticity?: number;
+}): void;
 
-    function easeOut(config?: { friction?: number }): void;
+function easeOut(config?: { friction?: number }): void;
 
-    function easeIn(config?: { friction?: number }): void;
+function easeIn(config?: { friction?: number }): void;
 
-    function easeInOut(config?: { friction?: number }): void;
+function easeInOut(config?: { friction?: number }): void;
 
-    function linear(): void;
+function linear(): void;
 
-    function bezier(config?: { points: any[] }): void;
+function bezier(config?: { points: any[] }): void;
 
-    function setTimeout(fn: (...args: any[]) => void, delay: number): number;
+function setTimeout(fn: (...args: any[]) => void, delay: number): number;
 
-    function clearTimeout(id: number): void;
+function clearTimeout(id: number): void;
 
-    function stop<P>(target: P): void;
+function stop<P>(target: P): void;
 
-    function toggleSlow(): void;
+function toggleSlow(): void;
 
-    function css<T extends HTMLElement>(el: T, properties: T["style"]): number;
-  }
+function css<T extends HTMLElement>(el: T, properties: T["style"]): number;
+}
 
-  export default dynamics;
+export default dynamics;
